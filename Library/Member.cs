f7d64a9de7d445e645260749038e4bdaab8712d8
Jeff John <John-J8@ulster.ac.uk>
2024-10-08 10:45:52 +0100

    Updated code with a new member class.

---
+++ Library/Member.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Sockets;
+
+namespace Library
+{
+    class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        public int MemberID
+        {
+            get { return memberId; }
+            private set { memberId = value; } // Private setter makes it read-only 
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        // Constructor for new member
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.memberId = memberId;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {MemberID}");
+            Console.WriteLine($"Member Name: {Name}");
+            Console.WriteLine($"Member Address: {Address}");
+            Console.WriteLine($"Member Phone No: {Phone}");
+            Console.WriteLine();
+        }
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -8,9 +8,20 @@ class Program
         // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
         Book book = new Book("C# for beginner", "Bill Gates", "1234567");
+        Book book1 = new Book("Ultimate C#", "Microsoft", "2233445");
 
+        Console.WriteLine("Currently available books");
 
         // Output the information to the console
         book.DisplayInfo();
+        book1.DisplayInfo();
+
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current library members");
+
+        member.DisplayInfo();
+        member1.DisplayInfo();
     }
 }
